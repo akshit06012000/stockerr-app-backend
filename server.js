444cc433aaca6b9156eeb00a661129eea327fc38
akshit06012000 <96385266+akshit06012000@users.noreply.github.com>
2022-12-30 11:27:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["https://stockker-app.vercel.app"],
+    origin: ["http://localhost:3000","https://stockker-app.vercel.app"],
     credentials: true,
   })
 );
