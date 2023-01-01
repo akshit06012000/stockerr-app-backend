b3f91ed587fcc458a53df4368f09c3a9ac2a332c
akshit06012000 <96385266+akshit06012000@users.noreply.github.com>
2023-01-01 14:36:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use(bodyParser.json());
   })
 );*/
 
+app.use(cors())
+
 app.use(function (request, response, next) {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
