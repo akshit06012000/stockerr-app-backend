1d17f1b8462ed7197272693bc76db941f22719ff
akshit06012000 <96385266+akshit06012000@users.noreply.github.com>
2023-01-01 15:33:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors())
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
